86ea906eb144e03454faca19d3779a5905a2314e
Luke Young <bored-engineer@users.noreply.github.com>
2024-12-28 22:10:11 -0800

    Update safe.go

--- safe.go
+++ safe.go
@@ -7,11 +7,21 @@ func (s stringConst) ToSql() (sqlStr string, args []interface{}, err error) {
 	return string(s), nil, nil
 }
 
-// DangerouslyCastDynamicStringToSQL converts a dynamic string to a stringConst for use in the methods/types of this package.
+// StringConst allows callers to explicitly declare a 'stringConst'
+func StringConst(val stringConst) stringConst {
+	return val
+}
+
+// StringConst allows callers to explicitly declare multiple 'stringConst's
+func StringConsts(vals ...stringConst) []stringConst {
+	return vals
+}
+
+// DangerouslyCastDynamicStringToStringConst converts a dynamic string to a stringConst for use in the methods/types of this package.
 // This should be used with _extreme_ caution, as it will lead to SQL injection if the string has not been properly sanitized.
 //
 // Deprecated: This function is dangerous and should not be used unless you are _very_ sure you know what you're doing.
-func DangerouslyCastDynamicStringToSQL(val string) stringConst {
+func DangerouslyCastDynamicStringToStringConst(val string) stringConst {
 	return stringConst(val)
 }
 
